cf6da99f5062ce48e23a3c5b80b23d0e795c0252
nggr51 <35467461+nggr51@users.noreply.github.com>
2023-01-13 15:54:45 +0100

    Add basic functionality

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-fetch("https://www.thecolorapi.com/id?hex=a604D3&format=json")
-	.then((res) => res.json())
-	.then((color) => {
-		console.log(color);
-		document.getElementById(
-			"container"
-		).innerHTML = `<img src="${color.image.named}">`;
-	});
+const delay = 200; // delay in ms
+const colorInput = document.getElementById("color-input");
+const generateBtn = document.getElementById("generate-btn");
+
+const column1El = document.getElementById("column-1");
+const column2El = document.getElementById("column-2");
+const column3El = document.getElementById("column-3");
+const column4El = document.getElementById("column-4");
+const column5El = document.getElementById("column-5");
+
+function renderColors() {
+	column1El.getElementsByTagName("div")[0].style.backgroundColor =
+		colorInput.value;
+
+	choosenColorHex = colorInput.value.slice(1);
+
+	fetch(`https://www.thecolorapi.com/scheme?hex=${choosenColorHex}&count=4`)
+		.then((res) => res.json())
+		.then((scheme) => {
+			setTimeout(() => {
+				column2El.getElementsByClassName(
+					"color-field"
+				)[0].style.backgroundColor = scheme.colors[0].hex.value;
+				setTimeout(() => {
+					column3El.getElementsByClassName(
+						"color-field"
+					)[0].style.backgroundColor = scheme.colors[1].hex.value;
+					setTimeout(() => {
+						column4El.getElementsByClassName(
+							"color-field"
+						)[0].style.backgroundColor = scheme.colors[2].hex.value;
+						setTimeout(() => {
+							column5El.getElementsByClassName(
+								"color-field"
+							)[0].style.backgroundColor = scheme.colors[3].hex.value;
+						}, delay);
+					}, delay);
+				}, delay);
+			}, delay);
+		});
+}
+
+generateBtn.addEventListener("click", () => {
+	renderColors();
+	console.log(colorInput.value);
+});
